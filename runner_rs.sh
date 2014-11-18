78b74f9fbd765695f4dce9b076987ce37aa07c8b
Cloud User <ec2-user@ip-10-93-7-23.ec2.internal>
2014-11-17 19:52:36 -0500

    getting scripts to work

--- runner_rs.sh
+++ runner_rs.sh
@@ -6,14 +6,11 @@ then
   SUITE="sanity"
 fi
 
-OUTDIR=/data3/db/logs
 DATADEV=/dev/xvde
-JOURDEV=/dev/xvde
 LOGDEV=/dev/xvdf
 MONGO_ROOT=/home/ec2-user
 DBPATH=/data2/db
 LOGPATH=/data3/db
-JOURPATH=/data2/db
 RH=32
 
 MONGO_OPTIONS=""
@@ -22,23 +19,18 @@ echo "never" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
 echo "never" | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
 echo "0" | sudo tee /proc/sys/kernel/randomize_va_space
 
-for VER in "2.6.5";  do
-  for STORAGE_ENGINE in  "mmapv0" "mmapv1" "wiredtiger" ; do
-    for RS_CONF in "none" "single" "set"; done
+#for VER in "2.6.5" "2.8.0-rc0"  ;  do
+for VER in "bec3fc4b1374fb9eff9e4cc82ada7b8d5f43de45-2014-11-17"  ;  do
+#  for STORAGE_ENGINE in "wiredtiger" "mmapv1" "mmapv0" ; do
+  for STORAGE_ENGINE in "wiredtiger" "mmapv1" ; do
+#    for RS_CONF in "set" "none" "single" ; do
+    for RS_CONF in "single" ; do
       killall mongod
       echo "3" | sudo tee /proc/sys/vm/drop_caches
       sudo blockdev --setra $RH $DATADEV
-      sudo blockdev --setra $RH $JOURDEV
       sudo blockdev --setra $RH $LOGDEV
-      rm -r $JOURPATH/journal
-      rm -r $DBPATH/*
-      rm $LOGPATH/server.log
-
-      if [ "$JOURPATH" != "$DBPATH" ]
-          then
-          mkdir -p $JOURPATH/journal
-          ln -s $JOURPATH/journal $DBPATH/journal
-      fi
+      rm -r $DBPATH/
+      rm -r $LOGPATH/
 
       MONGOD=$MONGO_ROOT/mongodb-linux-x86_64-$VER/bin/mongod
       MONGO=$MONGO_ROOT/mongodb-linux-x86_64-$VER/bin/mongo
@@ -48,10 +40,10 @@ for VER in "2.6.5";  do
         continue;
       fi
 
-      SE_SUPPORTED=$MONGOD --help | grep -i storageEngine | wc -l
-      if [ "$STORAGE_ENGINE" = "mmapv0" ] && [ "$SE_SUPPORTED" == 1 ]
+      SE_SUPPORTED=`$MONGOD --help | grep -i storageEngine | wc -l`
+      if [ "$SE_SUPPORTED" == 0 ] && [ "$STORAGE_ENGINE" != "mmapv0" ]
       then
-          continue;
+        continue;
       fi
       
       if [ "$SE_SUPPORTED" == 1 ]
@@ -63,44 +55,46 @@ for VER in "2.6.5";  do
       
       if [ "$STORAGE_ENGINE" == "wiredtiger" ]
       then
-        SE_CONF='--wiredTigerEngineConfig "checkpoint=(wait=14400)"'
+        SE_CONF="--wiredTigerEngineConfig 'checkpoint=(wait=14400)'"
       else
-        SE_CONF="--syncdelay=36000"
+        SE_CONF="--syncdelay 14400"
       fi
       
       if [ "$RS_CONF" == "single" ]
       then
+#        RS_EXTRA="--replSet mp --oplogSize 500"
         RS_EXTRA="--master --oplogSize 500"
       elif [ "$RS_CONF" == "set" ]
       then
-        RS_EXTRA="-replSet mp --oplogSize 500"
+        RS_EXTRA="--replSet mp --oplogSize 500"
       else
         RS_EXTRA=""
       fi
            
       # start the primary
-      mkdir $DBPATH/db100
-      mkdir $LOGPATH/db100
-      numactl --physcpubind=16-23 --interleave=all $MONGOD --port 27017 --dbpath $DBPATH/db100 --logpath $LOGPATH/db100/server.log --fork $MONGO_OPTIONS $SE_OPTION $SE_CONF $RS_EXTRA
+      mkdir -p $DBPATH/db100
+      mkdir -p $LOGPATH/db100
+      (eval numactl --physcpubind=16-23 --interleave=all $MONGOD --port 27017 --dbpath $DBPATH/db100 --logpath $LOGPATH/db100/server.log --fork $MONGO_OPTIONS $SE_OPTION $SE_CONF $RS_EXTRA )
       sleep 20
-      $MONGOD -eval "use admin; rs.initiate()"
-      $MONGOD -eval "use admin; rs.add('localhost:27018')"
-      $MONGOD -eval "use admin; rs.add('localhost:27019')"
-      
       # start other members (if needed)
+      if [ "$RS_CONF" == "single" ]
+      then
+echo      ${MONGO} --quiet --eval 'rs.initiate( ); while (rs.status().startupStatus || (rs.status().hasOwnProperty("myState") && rs.status().myState != 1)) { sleep(1000); };'
+      fi
       if [ "$RS_CONF" == "set" ]
       then
-        mkdir $DBPATH/db200
-        mkdir $LOGPATH/db200
-        numactl --physcpubind=8-15 --interleave=all $MONGOD --port 27018 --dbpath $DBPATH/db200 --logpath $LOGPATH/db200/server.log --fork $MONGO_OPTIONS $SE_OPTION $SE_CONF $RS_EXTRA
-        mkdir $DBPATH/db300
-        mkdir $LOGPATH/db300
-        numactl --physcpubind=24-31 --interleave=all $MONGOD --port 27019 --dbpath $DBPATH/db300 --logpath $LOGPATH/db300/server.log --fork $MONGO_OPTIONS $SE_OPTION $SE_CONF $RS_EXTRA
+        mkdir -p $DBPATH/db200
+        mkdir -p $LOGPATH/db200
+        (eval numactl --physcpubind=8-15 --interleave=all $MONGOD --port 27018 --dbpath $DBPATH/db200 --logpath $LOGPATH/db200/server.log --fork $MONGO_OPTIONS $SE_OPTION $SE_CONF $RS_EXTRA )
+        mkdir -p $DBPATH/db300
+        mkdir -p $LOGPATH/db300
+        (eval numactl --physcpubind=24-31 --interleave=all $MONGOD --port 27019 --dbpath $DBPATH/db300 --logpath $LOGPATH/db300/server.log --fork $MONGO_OPTIONS $SE_OPTION $SE_CONF $RS_EXTRA )
         sleep 20
-
+        ${MONGO} --quiet --eval 'var config = { _id: "mp", members: [ { _id: 0, host: "ip-10-93-7-23.ec2.internal:27017",priority:10 }, { _id: 1, host: "ip-10-93-7-23.ec2.internal:27018" }, { _id: 3, host: "ip-10-93-7-23.ec2.internal:27019" } ],settings: {chainingAllowed: false} }; rs.initiate( config ); while (rs.status().startupStatus || (rs.status().hasOwnProperty("myState") && rs.status().myState != 1)) { sleep(1000); };' 
       fi
       # start mongo-perf
-      taskset -c 0-7 python benchrun.py -f testcases/test.js -t 1 2 4 8 12 16 20 -l $SUITE-$VER-$STORAGE_ENGINE-$RS_CONF --rhost "54.191.70.12" --rport 27017 -s ../mongo/bin/mongo 
-      --mongo-repo-path /home/ec2-user/mongo-src --writeCmd true --trialCount 1 --testFilter $SUITE
+#      taskset -c 0-7 python benchrun.py -f testcases/*.js -t 1 2 4 8 12 16 20 -l $SUITE-$VER-$STORAGE_ENGINE-$RS_CONF --rhost "54.191.70.12" --rport 27017 -s ../mongo/mongo --mongo-repo-path /home/ec2-user/mongo --writeCmd true --trialCount 1 --nodyno --testFilter "{include: ['sanity'], exclude: ['Mixed.v0.FineThenUpdate-50-50','Insert.JustID','Insert.IntID','Insert.IntIDUpsert','Insert.JustNum','insert']}"
+      taskset -c 0-7 python benchrun.py -f testcases/*.js -t 1 2 4 8 12 16 20 -l $SUITE-$VER-$STORAGE_ENGINE-$RS_CONF --rhost "54.191.70.12" --rport 27017 -s ../mongo/mongo --mongo-repo-path /home/ec2-user/mongo --writeCmd true --trialCount 1 --nodyno 
+    done
   done
 done
