b57b462f15a85104f6d98561336c9755378a8910
Alvin Richards <alvin@10gen.com>
2014-12-04 14:23:52 -0800

    remove old files

--- nway-blacklist.js
+++
@@ -1,6 +0,0 @@
-function addBlacklisted(predicate) {
-    var blacklisted = {test: {$nin: ["Commands.v1.DistinctWithoutIndex","Commands.v1.DistinctWithoutIndexAndQuery","Commands.isMaster"]}};
-    for (var attrname in predicate) { blacklisted[attrname] = predicate[attrname]; };
-
-    return blacklisted;
-}
