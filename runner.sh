5bb5b8d8f519e87b3d275d7a7c4c1f51faa144bc
Alvin Richards <alvin@mongodb.com>
2014-12-09 16:21:09 -0800

    bring up

--- runner.sh
+++ runner.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 SUITE=$1
+LABEL=$2
 
 if [ "$SUITE" = "" ]
 then
   SUITE="sanity"
+fi
+
+if [ "$LABEL" = "" ]
+then
+  LABEL=$SUITE
 fi
 
 OUTDIR=/data3/logs
@@ -13,27 +19,30 @@ LOGDEV=/dev/sde
 MONGO_ROOT=/home/alvin
 DBPATH=/ssd-1tb/db
 LOGPATH=/data3/db
-JOURPATH=/data/db
+#JOURPATH=/data/db
+JOURPATH=${DBPATH}
 RH=32
 MONGO_OPTIONS=""
 
-BENCHRUN_OPTS="--nodyno --testFilter='$SUITE'"
+EXTRA_OPTS="--nodyno --testFilter='$SUITE'"
 
 echo "never" | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
 echo "never" | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
 
 #for VER in "2.6.5" "bddcc15df9736883f8b94e18a8718b24079b1c94-2014-11-17" ; do
-for VER in "534263f1d83cdeb142c27f0ea5a1ecffc5b7526a-2014-11-21"  ; do
+#for VER in "534263f1d83cdeb142c27f0ea5a1ecffc5b7526a-2014-11-21"  ; do
+for VER in "ee6fa9cf4870f81de1a4005cce2be6a91ac551ac-2014-12-06" ; do
 
   MONGOD=$MONGO_ROOT/mongodb-linux-x86_64-$VER/bin/mongod
 
   if [  ! -f "$MONGOD" ]
   then
+    echo $MONGOD does not exist
     continue
   fi
 
-  for STORAGE_ENGINE in "mmapv0" "mmapv1" "wiredtiger" ; do
-    for BENCHRUN_OPTS in "-m 8" "-c 1" "-c 8" ; do
+  for STORAGE_ENGINE in "mmapv0" "mmapv1" "wiredTiger" ; do
+    for BENCHRUN_OPTS in "-c 8" "-c 1" "-m 8"; do
 
       SE_SUPPORT=$($MONGOD --help | grep storageEngine | wc -l)
 
@@ -54,9 +63,9 @@ for VER in "534263f1d83cdeb142c27f0ea5a1ecffc5b7526a-2014-11-21"  ; do
         SE_EXTRA=""
       fi
 
-      if [ "$STORAGE_ENGINE" = "wiretiger" ]
+      if [ "$STORAGE_ENGINE" = "wiredTiger" ]
       then
-        MONGO_EXTRA='--wiredTigerEngineConfig "checkpoint=(wait=4800)"'
+        MONGO_EXTRA='--wiredTigerEngineConfig checkpoint=(wait=4800)'
       else
          MONGO_EXTRA="--syncdelay 4800"
       fi
@@ -80,7 +89,7 @@ for VER in "534263f1d83cdeb142c27f0ea5a1ecffc5b7526a-2014-11-21"  ; do
       sleep 20
 
       CONFIG=`echo $BENCHRUN_OPTS| tr -d ' '`
-      taskset 0xf00 python benchrun.py -f testcases/* -t 1 2 4 8 12 16 20 -l $SUITE-$VER-$STORAGE_ENGINE$CONFIG --rhost 54.191.70.12 --rport 27017 -s ../mongo-perf-shell/mongo --mongo-repo-path /home/alvin/mongo --writeCmd true --trialCount 1 $BENCHRUN_OPTS
+      taskset 0xf00 python benchrun.py -f testcases/* -t 1 2 4 8 12 16 20 -l $LABEL-$VER-$STORAGE_ENGINE$CONFIG --rhost 54.191.70.12 --rport 27017 -s ../mongo-perf-shell/mongo --mongo-repo-path /home/alvin/mongo --writeCmd true --trialCount 1 $BENCHRUN_OPTS $EXTRA_OPTS
      done
   done
 done
